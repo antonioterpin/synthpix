a75a9a249a77acf0a9395a61bfa473601339de5f
Francesco Banelli <fbanelli@ethz.ch>
2025-04-09 23:56:55 +0200

    feat: main file for demo

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
-import timeit
+"""Main file to run the SyntheticImageSampler pipeline."""
 import argparse
-import matplotlib.pyplot as plt
 import os
 
+import matplotlib.pyplot as plt
+
+from src.sym.data_generate import generate_images_from_flow
 from src.sym.image_sampler import SyntheticImageSampler
 from src.sym.scheduler import HDF5FlowFieldScheduler
-from src.sym.processing import generate_images_from_flow
 from src.utils import logger
 
-def visualize_and_save(batch, output_dir, num_images_to_display=5):
+
+def visualize_and_save(batch, output_dir="output_images", num_images_to_display=5):
     """Visualizes and saves a specified number of images from a batch.
 
     Args:
@@ -32,32 +34,112 @@ def visualize_and_save(batch, output_dir, num_images_to_display=5):
         axes[2].imshow(flow_field[..., 0], cmap="viridis")
         axes[2].set_title("Flow Field")
 
-        plt.tight_layout()
         plt.savefig(os.path.join(output_dir, f"batch_image_{i}.png"))
-        plt.close(fig)
 
-    print(f"Saved {min(num_images_to_display, len(images1))} images to {output_dir}")
+    logger.info(
+        f"Saved {min(num_images_to_display, len(images1))} images to {output_dir}"
+    )
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Demo for SyntheticImageSampler pipeline.")
+    """Main function to run the SyntheticImageSampler pipeline."""
+    parser = argparse.ArgumentParser(
+        description="Demo for SyntheticImageSampler pipeline."
+    )
 
     # Add arguments for SyntheticImageSampler parameters
-    parser.add_argument("--scheduler_files", nargs='+', required=True, help="List of HDF5 files for the scheduler.")
-    parser.add_argument("--images_per_field", type=int, default=1000, help="Number of synthetic images to generate per flow field.")
-    parser.add_argument("--batch_size", type=int, default=250, help="Number of synthetic images per batch.")
-    parser.add_argument("--image_shape", type=int, nargs=2, default=(1216, 1936), help="Shape of the synthetic images.")
-    parser.add_argument("--position_bounds", type=int, nargs=2, default=(1536, 2048), help="Shape of the big image from which the flow field is sampled.")
-    parser.add_argument("--num_particles", type=int, default=40000, help="Number of particles to simulate.")
-    parser.add_argument("--p_hide_img1", type=float, default=0.01, help="Probability of hiding particles in the first image.")
-    parser.add_argument("--p_hide_img2", type=float, default=0.01, help="Probability of hiding particles in the second image.")
-    parser.add_argument("--diameter_range", type=float, nargs=2, default=(0.1, 1.0), help="Range of diameters for particles.")
-    parser.add_argument("--intensity_range", type=float, nargs=2, default=(50, 200), help="Range of intensities for particles.")
-    parser.add_argument("--rho_range", type=float, nargs=2, default=(-0.99, 0.99), help="Range of correlation coefficients for particles.")
-    parser.add_argument("--dt", type=float, default=1.0, help="Time step for the simulation.")
+    parser.add_argument(
+        "--scheduler_files",
+        nargs="+",
+        required=True,
+        help="List of HDF5 files for the scheduler.",
+    )
+    parser.add_argument(
+        "--images_per_field",
+        type=int,
+        default=1000,
+        help="Number of synthetic images to generate per flow field.",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=250,
+        help="Number of synthetic images per batch.",
+    )
+    parser.add_argument(
+        "--image_shape",
+        type=int,
+        nargs=2,
+        default=(1216, 1936),
+        help="Shape of the synthetic images.",
+    )
+    parser.add_argument(
+        "--position_bounds",
+        type=int,
+        nargs=2,
+        default=(1536, 2048),
+        help="Shape of the big image from which the flow field is sampled.",
+    )
+    parser.add_argument(
+        "--num_particles",
+        type=int,
+        default=40000,
+        help="Number of particles to simulate.",
+    )
+    parser.add_argument(
+        "--p_hide_img1",
+        type=float,
+        default=0.01,
+        help="Probability of hiding particles in the first image.",
+    )
+    parser.add_argument(
+        "--p_hide_img2",
+        type=float,
+        default=0.01,
+        help="Probability of hiding particles in the second image.",
+    )
+    parser.add_argument(
+        "--diameter_range",
+        type=float,
+        nargs=2,
+        default=(2, 4),
+        help="Range of diameters for particles.",
+    )
+    parser.add_argument(
+        "--intensity_range",
+        type=float,
+        nargs=2,
+        default=(50, 200),
+        help="Range of intensities for particles.",
+    )
+    parser.add_argument(
+        "--rho_range",
+        type=float,
+        nargs=2,
+        default=(-0.99, 0.99),
+        help="Range of correlation coefficients for particles.",
+    )
+    parser.add_argument(
+        "--dt", type=float, default=1.0, help="Time step for the simulation."
+    )
     parser.add_argument("--seed", type=int, default=0, help="Random seed for JAX PRNG.")
-    parser.add_argument("--visualize", action="store_true", help="Enable visualization of generated images.")
-    parser.add_argument("--output_dir", type=str, default="output_images", help="Directory to save visualized images.")
-    parser.add_argument("--num_images_to_display", type=int, default=5, help="Number of images to display and save per batch.")
+    parser.add_argument(
+        "--visualize",
+        action="store_true",
+        help="Enable visualization of generated images.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="output_images",
+        help="Directory to save visualized images.",
+    )
+    parser.add_argument(
+        "--num_images_to_display",
+        type=int,
+        default=5,
+        help="Number of images to display and save per batch.",
+    )
 
     # Add a predefined configuration for JHTDB
     PREDEFINED_CONFIGS = {
@@ -79,7 +161,12 @@ def main():
     }
 
     # Add argument for predefined configuration
-    parser.add_argument("--config", type=str, choices=PREDEFINED_CONFIGS.keys(), help="Use a predefined configuration.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        choices=PREDEFINED_CONFIGS.keys(),
+        help="Use a predefined configuration.",
+    )
 
     args = parser.parse_args()
 
@@ -123,6 +210,7 @@ def main():
 
         if i >= args.images_per_field // args.batch_size - 1:
             break
+
 
 if __name__ == "__main__":
     main()
