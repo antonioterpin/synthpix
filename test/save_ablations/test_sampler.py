f2ff29296f2c6402044c3ba67db1c37a1240e71e
Antonio Terpin <antonio.terpin@gmail.com>
2025-06-13 19:12:38 +0200

    chore: fix mock data creation and deletion

--- test/save_ablations/test_sampler.py
+++ test/save_ablations/test_sampler.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 from synthpix.data_generate import generate_images_from_flow
-from synthpix.image_sampler import SyntheticImageSampler
+from synthpix.sampler import SyntheticImageSampler
 from synthpix.scheduler import PrefetchingFlowFieldScheduler
 from synthpix.utils import load_configuration
 
