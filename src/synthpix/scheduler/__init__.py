bdcb27f3302511abc39cd211703589ed4d9eb71f
Francesco Banelli <francesco.banelli@hotmail.it>
2025-07-25 18:32:55 +0200

    fix: remove flo from scheduler init

--- src/synthpix/scheduler/__init__.py
+++ src/synthpix/scheduler/__init__.py
@@ -1,7 +1,6 @@
 """Flow field scheduler module."""
 from .base import BaseFlowFieldScheduler
 from .episodic import EpisodicFlowFieldScheduler
-from .flo import FloFlowFieldScheduler
 from .hdf5 import HDF5FlowFieldScheduler
 from .mat import MATFlowFieldScheduler
 from .numpy import NumpyFlowFieldScheduler
@@ -14,5 +13,4 @@ __all__ = [
     "MATFlowFieldScheduler",
     "EpisodicFlowFieldScheduler",
     "PrefetchingFlowFieldScheduler",
-    "FloFlowFieldScheduler",
 ]
